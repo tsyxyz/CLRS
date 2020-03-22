a9569d545a48f8336516025dee67fd22fb04c542
tsyxyz <tsyxyz@hotmail.com>
2020-03-22 15:10:59 +0800

    add insertion_sort of non increasing

--- ch02/include/ch02/insertion_sort.h
+++ ch02/include/ch02/insertion_sort.h
@@ -4,11 +4,24 @@
 
 namespace ch02 {
 template <typename Container>
-void insertion_sort(Container &seq) {
+void InsertionSortNonDecrease(Container &seq) {
   for (size_t j = 0; j < seq.size(); ++j) {
     auto key = seq[j];
     size_t i = j - 1;
     while (i >= 0 && seq[i] > key) {
+      seq[i + 1] = seq[i];
+      i = i - 1;
+    }
+    seq[i + 1] = key;
+  }
+}
+
+template <typename Container>
+void InsertionSortNonIncrease(Container &seq) {
+  for (size_t j = 0; j < seq.size(); ++j) {
+    auto key = seq[j];
+    size_t i = j - 1;
+    while (i >= 0 && seq[i] < key) {
       seq[i + 1] = seq[i];
       i = i - 1;
     }
--- ch02/test/insertion_sort_test.cpp
+++ ch02/test/insertion_sort_test.cpp
@@ -3,13 +3,24 @@
 #include <iostream>
 #include <vector>
 
-TEST(insertion_sort, case0) {
+TEST(insertion_sort, non_decrease) {
   std::vector<int> seq = {8, 1, 3, 0, 2, 7};
   std::vector<int> answer = {0, 1, 2, 3, 7, 8};
-  ch02::insertion_sort(seq);
+  ch02::InsertionSortNonDecrease(seq);
   // for (auto i : seq) {
   //   std::cout << i << " ";
   // }
   // std::cout << std::endl;
+  EXPECT_TRUE(seq == answer);
+}
+
+TEST(insertion_sort, non_increase) {
+  std::vector<int> seq = {8, 1, 3, 0, 2, 7};
+  std::vector<int> answer = {8, 7, 3, 2, 1, 0};
+  ch02::InsertionSortNonIncrease(seq);
+  for (auto i : seq) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
   EXPECT_TRUE(seq == answer);
 }
