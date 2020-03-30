5b035ee81867cb47132317959c0b0e5bca8dc205
tsyxyz <tsyxyz@hotmail.com>
2020-03-31 07:56:52 +0800

    clang format

--- ch02/test/linear_search_test.cpp
+++ ch02/test/linear_search_test.cpp
@@ -3,17 +3,17 @@
 #include <vector>
 
 TEST(linear_search, has_value) {
-    std::vector<int> seq = {8, 1, 3, 0, 2, 7};
-    int v = 2;
-    int answer = 4;
-    int result = ch02::LinearSearch(seq, v);
-    EXPECT_TRUE(result == answer);
+  std::vector<int> seq = {8, 1, 3, 0, 2, 7};
+  int v = 2;
+  int answer = 4;
+  int result = ch02::LinearSearch(seq, v);
+  EXPECT_TRUE(result == answer);
 }
 
 TEST(linear_search, has_no_value) {
-    std::vector<int> seq = {8, 1, 3, 0, 2, 7};
-    int v = 9;
-    int answer = -1;
-    int result = ch02::LinearSearch(seq, v);
-    EXPECT_TRUE(result == answer);
+  std::vector<int> seq = {8, 1, 3, 0, 2, 7};
+  int v = 9;
+  int answer = -1;
+  int result = ch02::LinearSearch(seq, v);
+  EXPECT_TRUE(result == answer);
 }
