f25550d21f1dc484dd7729cd6d10da18fc9abdaa
tsyxyz <tsyxyz@hotmail.com>
2020-05-19 07:44:04 +0800

    add 2.3-2

--- ch02/include/ch02/merge_sort.h
+++ ch02/include/ch02/merge_sort.h
@@ -38,7 +38,50 @@ void Merge(Container &seq, int p, int q, int r) {
 
 template <typename Container>
 void MergeNoSentinel(Container &seq, int p, int q, int r) {
+  int n1 = q - p + 1;
+  int n2 = r - q;
+  typename Container::value_type *L =
+      new typename Container::value_type[n1];
+  typename Container::value_type *R =
+      new typename Container::value_type[n2];
+  for (int i = 0; i < n1; ++i) {
+    L[i] = seq[p + i];
+  }
+  for (int j = 0; j < n2; ++j) {
+    R[j] = seq[q + j + 1];
+  }
+
+  int i = 0;
+  int j = 0;
+  int k = p;
+  for (; k <= r; ++k) {
+    if (i == n1 || j == n2) {
+      break;
+    }
+
+    if (L[i] <= R[j]) {
+      seq[k] = L[i];
+      i = i + 1;
+    } else {
+      seq[k] = R[j];
+      j = j + 1;
+    }
+  }
   
+  if (i == n1) {
+    for (; k <= r; ++k) {
+      seq[k] = R[j];
+      j = j + 1;
+    }
+  } else {
+    for (; k <= r; ++k) {
+      seq[k] = L[i];
+      i = i + 1;
+    }
+  }
+
+  delete[] L;
+  delete[] R;
 }
 
 template <typename Container>
@@ -48,6 +91,16 @@ void MergeSort(Container &seq, int p, int r) {
     MergeSort(seq, p, q);
     MergeSort(seq, q + 1, r);
     Merge(seq, p, q, r);
+  }
+}
+
+template <typename Container>
+void MergeSortNoSentinel(Container &seq, int p, int r) {
+  if (p < r) {
+    int q = int((p + r) / 2);
+    MergeSortNoSentinel(seq, p, q);
+    MergeSortNoSentinel(seq, q + 1, r);
+    MergeNoSentinel(seq, p, q, r);
   }
 }
 }  // namespace ch02
--- ch02/test/merge_sort_test.cpp
+++ ch02/test/merge_sort_test.cpp
@@ -17,3 +17,19 @@ TEST(merge_sort, sort) {
 
   EXPECT_TRUE(seq == answer);
 }
+
+TEST(merge_sort, merge_no_sentinel) {
+  std::vector<int> seq = {8, 2, 4, 5, 7, 1, 2, 3, 6, 0};
+  std::vector<int> answer = {8, 1, 2, 2, 3, 4, 5, 6, 7, 0};
+  ch02::MergeNoSentinel(seq, 1, 4, 8);
+
+  EXPECT_TRUE(seq == answer);
+}
+
+TEST(merge_sort, sort_no_sentinel) {
+  std::vector<int> seq = {5, 2, 4, 7, 1, 3, 2, 6};
+  std::vector<int> answer = {1, 2, 2, 3, 4, 5, 6, 7};
+  ch02::MergeSortNoSentinel(seq, 0, 7);
+
+  EXPECT_TRUE(seq == answer);
+}
